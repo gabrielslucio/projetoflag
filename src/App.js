6e2190d5abfcc73dae3b39f86d22f0bfe53f957e
Gabriel <gabrielsilvalucio@gmail.com>
2023-02-19 22:06:23 +0000

    Identação e update da navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import NavTop from './components/NavTop/NavTop';
 import NavBot from './components/NavBot/NavBot';
--- src/components/NavBot/NavBot.jsx
+++ src/components/NavBot/NavBot.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
+
 import '../NavBot/NavBot.scss';
+
 
 const NavBot = () => {
 
