b5df9727325fe9c07a343edddaa37e4568c90c39
Gabriel <gabrielsilvalucio@gmail.com>
2023-02-25 12:00:54 +0000

    Funcionalidade de caixa de comentários

--- src/Pages/ReservarLivros/RLivros.jsx
+++ src/Pages/ReservarLivros/RLivros.jsx
@@ -1,76 +1,123 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import {AiFillStar} from "react-icons/ai";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { AiFillStar } from 'react-icons/ai';
+import books from '../../constants/books';
+import images from '../../constants/images';
 
-import images from "../../constants/images";
-import books from "../../constants/books";
+const RLivros = () => {
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [comments, setComments] = useState([]);
 
 
+  useEffect(() => {
+    const bookData = books.find((book) => book.id === Number(id));
+    setBook(bookData);
 
-import './RLivros.scss'
-
-const RLivros = ({ match }) => {
-    
-    const { id } = useParams();
-    const [book, setBook] = useState(null);  
-    const [rating, setRating] = useState(0);
-    const [hover, setHover] = useState(0);
-
-    useEffect(() => {
-        const bookData = books.find((book) => book.id === Number(id));
-        setBook(bookData);
-    }, [id]);
-
-    if (!book) {
-        return <div>Loading...</div>
+    const storedComments = JSON.parse(localStorage.getItem(`book_${id}_comments`));
+    if (storedComments) {
+        setComments(storedComments);
     }
+  }, [id]);
+
+  useEffect(() => {
+    const storedComments = JSON.parse(localStorage.getItem(`book_${id}_comments`));
+    if (storedComments) {
+        setComments(storedComments);
+    }
+  })
+
+
+  useEffect(() => {
+    localStorage.setItem(`book-${id}-comments`, JSON.stringify(comments));
+  }, [comments, id]);
+
+  if (!book) {
+    return <div>Erro</div>;
+  }
+  
+
+  const handleCommentSubmit = (comment) => {
+    const updatedComments = [...comments, comment];
+    setComments(updatedComments);
+    localStorage.setItem(`book_${id}_comments`, JSON.stringify(updatedComments));
+  };
+
+  const CommentBox = ({ onSubmit }) => {
+    const [comment, setComment] = useState('');
+
+    const handleCommentChange = (event) => {
+      setComment(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      onSubmit(comment);
+      setComment('');
+    };
 
     return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Add a comment:
+            <textarea value={comment} onChange={handleCommentChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  };
 
-        <div className="app__rlivros">
-            <div className="app__rlivros-container">
-                <div className="app__rlivros-head">
-                    <h1>{book.name}</h1>
-                </div>
-
-                <div className="app__rlivros-livros">
-                    <h2>{book.author}</h2>
-                    <img 
-                    src={images[`book${book.id.toString().padStart(2, '0')}`]}
-                    alt={`Imagem do livro ${book.name}`}
-                    />
-                    <h3>Sinopse</h3>
-                    <p> 
-                        {book.description}
-                    </p>
-                </div>
-
-                <div className="app__rlivros-rating">
-                    <div className="app__rlivros-stars">
-                        {[...Array(5)].map((star, index) => {
-                            return (
-                                <button
-                                    type="button"
-                                    key={index}
-                                    className={index <= rating ? "on" : "off"}
-                                    onClick={() => setRating(index)}
-                                    onMouseEnter={() => setHover(index)}
-                                    onMouseLeave={() => setHover(rating)}
-                                >    
-                                    <span className="star"><AiFillStar /></span>
-                                </button>                            
-                            )
-                        })}
-                    </div>
-
-                    <div className="app__rlivros-comments">
-                        
-                    </div>
-                    
-                </div>
-            </div>            
+  return (
+    <div className="app__rlivros">
+      <div className="app__rlivros-container">
+        <div className="app__rlivros-head">
+          <h1>{book.name}</h1>
         </div>
-    )
-}
 
-export default RLivros
+        <div className="app__rlivros-livros">
+          <h2>{book.author}</h2>
+          <img
+            src={images[`book${book.id.toString().padStart(2, '0')}`]}
+            alt={`Imagem do livro ${book.name}`}
+          />
+          <h3>Synopsis</h3>
+          <p>{book.description}</p>
+        </div>
+
+        <div className="app__rlivros-rating">
+          <div className="app__rlivros-stars">
+            {[...Array(5)].map((star, index) => {
+              return (
+                <button
+                  type="button"
+                  key={index}
+                  className={index <= rating ? 'on' : 'off'}
+                  onClick={() => setRating(index)}
+                  onMouseEnter={() => setHover(index)}
+                  onMouseLeave={() => setHover(rating)}
+                >
+                  <span className="star">
+                    <AiFillStar />
+                  </span>
+                </button>
+              );
+            })}
+          </div>
+
+          <div className="app__rlivros-comments">
+            <CommentBox onSubmit={handleCommentSubmit} />
+            {comments.map((comment, index) => (
+              <div key={index}>{comment}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RLivros;
---
+++ src/constants/ppictures.js
@@ -0,0 +1,14 @@
+import pcat01 from '../assets/pcat01.jpg';
+import pcat02 from '../assets/pcat02.jpg';
+import pcat03 from '../assets/pcat03.jpg';
+import pcat04 from '../assets/pcat04.jpg';
+import pcat05 from '../assets/pcat05.jpg';
+
+
+export default {
+    pcat01,
+    pcat02,
+    pcat03,
+    pcat04,
+    pcat05
+}
