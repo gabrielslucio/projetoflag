f06496e585b07400c327ca855a77c6982b091271
Gabriel <gabrielsilvalucio@gmail.com>
2023-02-25 13:22:01 +0000

    Routing em Livros update

--- src/container/Livros/Livros.jsx
+++ src/container/Livros/Livros.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 import {BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs';
-
 
 import images  from '../../constants/images';
 
 import './Livros.scss';
 
-const booksImages = [images.book01, images.book02, images.book03, images.book04];
-
 const Livros = () => {
 
     const scrollRef = React.useRef(null);
+
+    const books = [
+        { id: 1, name: 'Book 1'},
+        { id: 2, name: 'Book 2' },
+        { id: 3, name: 'Book 3' },
+        { id: 4, name: 'Book 4' },
+    ]
 
     const scroll = (direction) => {
         const {current} = scrollRef;
@@ -25,12 +29,19 @@ const Livros = () => {
         }
     };
 
+   const navigate = useNavigate();
+
+   const selectedBooks = books.slice(0, 4);
+
+   const handleReserveClick = (book) => {
+    navigate(`/livro/${book.id}`);
+   };
+
     return (
         <div className="app__livros" id="app-livros">
             <div className="app__livros-content">
                 <h1 className="headtext">Livros</h1>
                 <p>Visita a nossa biblioteca e escolha um livro para levar consigo!</p>
-                
                 <NavLink className="app__livros-button" to={"/llivros"}>
                     <button type="button" className="app__livros-button">Reservar</button>
                 </NavLink>
@@ -38,17 +49,12 @@ const Livros = () => {
 
             <div className="app__livros-images">
                 <div className="app__livros-container" ref={scrollRef}>
-                   {[images.book01, images.book02, images.book03, images.book04]
-                   .map((image, index) => (
-                     <div className="app__livros-cards" 
-                        key={`gallery_image-${index + 1}`}> 
-                        <NavLink to={"/rlivros"}>
-                            <img src={image} alt="imagens dos livros"/>
-                        </NavLink>
-                     </div>
-                   ))}
-                </div>
-
+                    {selectedBooks.map((book, index) => (
+                        <div className="app__livros-cards" key={`gallery_image-${index + 1}`}>
+                            <img src={images[`book${book.id.toString().padStart(2, '0')}`]} alt={`Imagem do livro ${book.name}`} onClick={() => handleReserveClick(book)} />
+                        </div>
+                    ))}
+            </div>
                 <div className="app__livros-arrows">
                     <BsArrowLeftShort 
                         className="app__arrow-left" 
